9945507b0e10388750c4d8fd6ba6930810cd6004
Frenadol <fernandoramajimenez@gmail.com>
2024-05-24 13:06:59 +0200

    TODO DOCUMENTADO

--- src/main/java/com/github/JoseAngelGiron/DistanciaEuclidea.java
+++ src/main/java/com/github/JoseAngelGiron/DistanciaEuclidea.java
@@ -1,6 +1,12 @@
 package com.github.JoseAngelGiron;
 
 public class DistanciaEuclidea extends Distancia{
+    /**
+     * Calcula la distancia euclidiana entre dos puntos en un espacio bidimensional.
+     * @param origen El punto de origen.
+     * @param destino El punto de destino.
+     * @return La distancia euclidiana entre el punto de origen y el punto de destino.
+     */
     @Override
     public double medir(Punto origen, Punto destino) {
         double distancia = Math.sqrt(Math.pow(destino.getX() - origen.getX(), 2) + Math.pow(destino.getY() - origen.getY(), 2));
--- src/main/java/com/github/JoseAngelGiron/TransportistaSeul.java
+++ src/main/java/com/github/JoseAngelGiron/TransportistaSeul.java
@@ -3,9 +3,19 @@ package com.github.JoseAngelGiron;
 import javafx.scene.paint.Color;
 
 public class TransportistaSeul extends Transportista {
+
     public TransportistaSeul(String nombre, String color, Distancia medida) {
         super("fede", "Naranja", medida);
     }
+    /**
+     * Calcula el costo de envío basado en la distancia euclidiana entre dos puntos y el peso del envío.
+     *
+     * @param origen El punto de origen del envío.
+     * @param destino El punto de destino del envío.
+     * @param peso El peso del envío.
+     * @return El costo del envío basado en la distancia y el peso.
+     * @throws IllegalArgumentException Si la distancia de envío supera los 100 unidades.
+     */
     @Override
     public double presupuestar(Punto origen, Punto destino, double peso) {
         DistanciaEuclidea distanciaEuclidea = new DistanciaEuclidea();
